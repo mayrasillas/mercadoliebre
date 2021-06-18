a75e230125b078a3c01b8245e7ef6ae2ff35b1a5
mayrasillas <mayrasillas.c@hotmail.com>
2021-06-17 19:10:04 -0500

    modificado app.js

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-const publicPath = path.resolve(__dirname, './public');
-app.use(express.static(publicPath));
-
-app.listen(3003, () => console.log('Servidor corriendo'));
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+app.use(express.static(__dirname + '/public'))
+app.use(express.urlencoded({
+  extended: true
+}))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
+  res.sendFile('./home.html', { root: 'views'})
+})
+const port = process.env.PORT || 3000
+app.listen(port , () => {
+  console.log(`App listening at http://localhost:${port}`)
 })
